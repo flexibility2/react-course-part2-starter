194f07955acfd012dcb95e0979ac1590fea26a53
Xiaotian Wu <729406307@qq.com>
2024-07-20 21:26:38 +0800

    useMutate call back to update data

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,29 +3,48 @@ import { useRef } from "react";
 import { Todo } from "../hooks/useTodos";
 import axios from "axios";
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
   const ref = useRef<HTMLInputElement>(null);
 
-  const addMutate = useMutation<Todo, Error, Todo>({
+  const addMutate = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo[]>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
+
+    onMutate: (newTodo: Todo) => {
+      queryClient.cancelQueries(["todos"]);
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"] || []);
+      queryClient.setQueryData<Todo[]>(["todos"], (old) => [
+        newTodo,
+        ...(old || []),
+      ]);
+      return { previousTodos };
+    },
+
     onSuccess: (saveTodo, newTodo) => {
       console.log("saveTodo: ", saveTodo);
       console.log("newTodo: ", newTodo);
       // queryClient.invalidateQueries({
       //   queryKey: ["todos"],
       // });
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        saveTodo,
-        ...(todos || []),
-      ]);
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo.id === saveTodo.id ? saveTodo : todo))
+      );
 
       if (ref.current) {
         ref.current.value = "";
       }
+    },
+
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+      queryClient.setQueryData<Todo[]>(["todos"], context?.previousTodos);
     },
   });
   return (
